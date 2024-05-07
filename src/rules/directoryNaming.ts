397be8aba0ed1e0da290e09a1a93ec1162754f51
Milena Boselli Rosa <milena.3p@alloy.com>
2024-05-07 12:23:54 -0300

    fix enforce directory names rule with relative paths.

--- src/rules/directoryNaming.ts
+++ src/rules/directoryNaming.ts
@@ -1,21 +1,27 @@
 import { Rule } from 'eslint';
-import path from 'path';
+import { relative, parse, sep } from 'path';
 import { isKebabCase } from '../helpers/casing';
 
 const directoryNamingRule: Rule.RuleModule = {
   create(context) {
+    const projectRoot = process.cwd();
+
     return {
       Program: (node) => {
-        const { filename, cwd } = context;
-        const directoryPath = path.dirname(filename).replace(cwd, '');
+        const { filename } = context;
+        if (filename === '<input>' || filename === '<text>') {
+          // Skip virtual filenames used by ESLint in editor integrations
+          return;
+        }
 
+        const relativePath = parse(relative(projectRoot, filename)).dir;
         // Split the directory path to check each part
-        const parts = directoryPath.split(path.sep);
+        const parts = relativePath.split(sep);
         for (const part of parts) {
           if (part && !isKebabCase(part)) {
             context.report({
               node,
-              message: `Directory name "${part}" should be in kebab-case.`
+              message: `"${projectRoot} -- ${relativePath}" - Directory name "${part}" should be in kebab-case.`
             });
           }
         }
--- tests/rules/directoryNaming.test.ts
+++ tests/rules/directoryNaming.test.ts
@@ -1,6 +1,8 @@
-// Example test using Jest
+import path from 'node:path';
 import { RuleTester } from 'eslint';
 import directoryNamingRule from '../../src/rules/directoryNaming';
+
+const projectRoot = process.cwd();
 
 const ruleTester = new RuleTester({
   parser: require.resolve('@typescript-eslint/parser'),
@@ -8,11 +10,16 @@ const ruleTester = new RuleTester({
 });
 
 ruleTester.run('directory-naming', directoryNamingRule, {
-  valid: [{ code: 'var foo = 1;', filename: 'my-project/integrations/my-module/index.js' }],
+  valid: [
+    {
+      code: 'var foo = 1;',
+      filename: path.join(projectRoot, '/my-project/integrations/my-module/index.js')
+    }
+  ],
   invalid: [
     {
       code: 'var foo = 1;',
-      filename: 'myProject/integrations/MyModule/index.js',
+      filename: path.join(projectRoot, '/myProject/integrations/MyModule/index.js'),
       errors: [
         { message: 'Directory name "myProject" should be in kebab-case.' },
         { message: 'Directory name "MyModule" should be in kebab-case.' }
